4c08d65e3204b811c8590c1d6ccc281d46071c89
Sander <hanakmumakil23@gmail.com>
2018-04-23 12:36:40 +0200

    Adds PCA to reduce dimensionality of our images

--- preprocessing.py
+++ preprocessing.py
@@ -1,22 +1,36 @@
-from scipy import stats
 import matplotlib.pyplot as plt
-from numpy import reshape, array, float16
 from PIL import Image
-from os import walk, path
+from os import walk
 import numpy as np
-import random
+from sklearn.decomposition import PCA
 
 
 def get_image_tuples():
-    train_tuples = []
+    train_vectors, train_labels = [], []
     for dirpath, dirnames, filenames in walk("./chars74k-lite"):
         for filename in filenames:
             pic = np.array(Image.open(dirpath+"/"+filename))
-            pic_as_array = np.reshape(pic, len(pic[0]) * len(pic)) # This would be 20*20=400 for us, but this is general
-            pic_as_array = np.vectorize(lambda p: p/255.0)(pic_as_array) # Map onto 0-1 interval
-            pic_array_and_label_tuple = (pic_as_array, filename[0])
-            train_tuples.append(pic_array_and_label_tuple)
-    return train_tuples
+
+            # Reshape to single vector, general statement but second arument could just be 20*20=400 for us
+            pic_as_array = np.reshape(pic, len(pic[0]) * len(pic))
+            # Map onto 0-1 interval using simple division TODO: map smarter way?
+            pic_as_array = np.vectorize(lambda p: p/255.0)(pic_as_array)
+
+            train_vectors.append(pic_as_array)
+            train_labels.append(filename[0])
+    return train_vectors, train_labels
+
+
+def pca_reduce_dims(image_vectors, new_n_features):
+    # Map dimensionality from 400 -> new_n_features using PCA
+    '''
+    If we want to reduce the dimensionality by projecting the original data onto a vector such that
+    the squared projection error is minimized in all directions, we can simply project the data onto the largest eigenvectors.
+    This is called Principal Component Analysis, such projection also decorrelates the feature space.
+    See:
+    '''
+    pca = PCA(n_components=new_n_features)
+    return pca.fit_transform(image_vectors)
 
 
 def display_image(image, original_width=20, original_height=20, zero_one_interval=True):
@@ -28,8 +42,15 @@ def display_image(image, original_width=20, original_height=20, zero_one_interva
 
 
 if __name__ == "__main__":
-    # A list of tuples that contain a single np.array of pixel values mapped onto interval 0-1 and a label as a string
-    train_tuples = get_image_tuples()
-    
+    # A list of np.array of 0-1 floats indicating pixel density for each on index i corresponds to label on index i
+    train_vectors, train_labels = get_image_tuples()
+
+    # Dimension of each np.array with floats reduced from 400 to 100
+    reduced_dim_train_vectors = pca_reduce_dims(train_vectors, 100)
+
+    # Combine to split data set for ML
+    train_tuples = np.array([(reduced_dim_train_vectors[i], train_labels[i] for i in range(len(train_labels)))])
+    train_set, test_set = train_tuples[:len(train_tuples)*0.8], train_tuples[train_tuples[:len(train_tuples)*0.8]:]
+
     # Test that we can map it back again:
-    display_image(train_tuples[0][0])
+    display_image(train_set[0][0])
