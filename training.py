389681f1cb937ef04242b273c4bd25214eda26dc
Sander <hanakmumakil23@gmail.com>
2018-04-24 16:03:36 +0200

    Changes inputs to model

--- training.py
+++ training.py
@@ -1,44 +1,31 @@
 import tensorflow as tf
 import numpy as np
-import _pickle as pickle
+from preprocessing import get_train_test_set
 
-with open("train_set.pickle", "rb") as p:
-    train_set = pickle.load(p)
-
-with open("test_set.pickle", "rb") as p:
-    test_set = pickle.load(p)
+train_set, test_set = get_train_test_set()
 
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True, allow_soft_placement=True))
 
 classifier = tf.estimator.DNNClassifier(
-    hidden_units=[75, 50, 35],
+    hidden_units=[321, 123, 32],
     n_classes=26,
-    feature_columns=[tf.feature_column.numeric_column(key=str(i)) for i in range(len(train_set[0][0]))],
+    feature_columns=[tf.feature_column.numeric_column(key="pixels", shape=[len(train_set[0][0]),], dtype=tf.float32, default_value=0.0)],
     model_dir="savedmodels/",
-    optimizer=tf.train.ProximalAdagradOptimizer(
-        learning_rate=0.1,
-        initial_accumulator_value=0.1,
-        l1_regularization_strength=0.1,
-        l2_regularization_strength=0.1,
+    dropout=0.1,
+    optimizer=tf.train.GradientDescentOptimizer(
+        learning_rate=0.2,
         use_locking=False,
-        name='ProximalAdagrad'
-    )
+        name='GradientDescent'
+    ),
 )
 
-train_features = {str(i): np.array([train_set[j][0][i] for j in range(len(train_set))]) for i in range(len(train_set[0][0]))}
-test_features = {str(i): np.array([train_set[j][0][i] for j in range(len(test_set))]) for i in range(len(test_set[0][0]))}
-train_labels = np.array([ord(train_set[i][1])-97 for i in range(len(train_set))])
-test_labels = np.array([ord(test_set[i][1])-97 for i in range(len(test_set))])
 
-#train_spec = tf.estimator.TrainSpec(input_fn=lambda:input_fn(train_features, train_labels, 128))
-train_input_fn = tf.estimator.inputs.numpy_input_fn({str(i): train_features[str(i)] for i in range(len(train_features))}, train_labels,
-                                    batch_size=128, num_epochs=1, shuffle=True, queue_capacity=512)
-eval_input_fn = tf.estimator.inputs.numpy_input_fn({str(i): test_features[str(i)] for i in range(len(test_features))}, test_labels,
-                                    batch_size=128, num_epochs=1, shuffle=False, queue_capacity=512)
-#eval_spec = tf.estimator.EvalSpec(input_fn=lambda:input_fn(test_features, test_labels, 128))
+train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"pixels": train_set[:, 0]}, y=train_set[:, 1].astype(np.int32),
+                                    batch_size=50, num_epochs=5, shuffle=True, queue_capacity=200)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn(x={"pixels": test_set[:, 0]}, y=test_set[:, 1].astype(np.int32),
+                                    batch_size=50, num_epochs=1, shuffle=False, queue_capacity=200)
 
 train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn)
 eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn)
 
 tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
-classifier.export_savedmodel("savedmodels/")
