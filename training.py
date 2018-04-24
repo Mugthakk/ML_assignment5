f2e735d7d29ba84a34e0d0f51e1c2ef9e73ae4b8
Sander <hanakmumakil23@gmail.com>
2018-04-24 13:08:22 +0200

    Now trains, but accuracy very low so probably a stupid fault somewhere

--- training.py
+++ training.py
@@ -13,7 +13,7 @@ sess = tf.Session(config=tf.ConfigProto(log_device_placement=True, allow_soft_pl
 classifier = tf.estimator.DNNClassifier(
     hidden_units=[75, 50, 35],
     n_classes=26,
-    feature_columns=[tf.feature_column.numeric_column(str(i)) for i in range(len(train_set[0][0]))],
+    feature_columns=[tf.feature_column.numeric_column(key=str(i)) for i in range(len(train_set[0][0]))],
     model_dir="savedmodels/",
     optimizer=tf.train.ProximalAdagradOptimizer(
         learning_rate=0.1,
@@ -27,8 +27,8 @@ classifier = tf.estimator.DNNClassifier(
 
 train_features = {str(i): np.array([train_set[j][0][i] for j in range(len(train_set))]) for i in range(len(train_set[0][0]))}
 test_features = {str(i): np.array([train_set[j][0][i] for j in range(len(test_set))]) for i in range(len(test_set[0][0]))}
-train_labels = np.array([train_set[i][1] for i in range(len(train_set))])
-test_labels = np.array([test_set[i][1] for i in range(len(test_set))])
+train_labels = np.array([ord(train_set[i][1])-97 for i in range(len(train_set))])
+test_labels = np.array([ord(test_set[i][1])-97 for i in range(len(test_set))])
 
 
 def input_fn(features, labels, batch_size):
@@ -37,8 +37,15 @@ def input_fn(features, labels, batch_size):
     return dataset
 
 
-train_spec = tf.estimator.TrainSpec(input_fn=lambda:input_fn(train_features, train_labels, 128))
-eval_spec = tf.estimator.EvalSpec(input_fn=lambda:input_fn(test_features, test_labels, 128))
+#train_spec = tf.estimator.TrainSpec(input_fn=lambda:input_fn(train_features, train_labels, 128))
+train_input_fn = tf.estimator.inputs.numpy_input_fn({str(i): train_features[str(i)] for i in range(len(train_features))}, train_labels,
+                                    batch_size=128, num_epochs=1, shuffle=True, queue_capacity=512)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn({str(i): test_features[str(i)] for i in range(len(test_features))}, test_labels,
+                                    batch_size=128, num_epochs=1, shuffle=False, queue_capacity=512)
+#eval_spec = tf.estimator.EvalSpec(input_fn=lambda:input_fn(test_features, test_labels, 128))
+
+train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn)
+eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn)
 
 tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
 classifier.export_savedmodel("savedmodels/")
