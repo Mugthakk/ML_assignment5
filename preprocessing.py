e1ed58023c3b583992f07e209b5342c7ead3486d
Carl O. Steen <carl_steen1@hotmail.com>
2018-04-27 14:50:57 +0200

    made svm less shitty and found the best parameters ;) <3

--- preprocessing.py
+++ preprocessing.py
@@ -55,11 +55,11 @@ def display_image(image, original_width=20, original_height=20, zero_one_interva
 
 
 def get_train_test_set():
-    train_vectors, train_labels = get_image_tuples(method="edge")
+    train_vectors, train_labels = get_image_tuples(method="hog")
 
     # Dimension of each np.array with floats reduced from 400 to 100
     reduced_dim_train_vectors = pca_reduce_dims(train_vectors, 100)
-    reduced_dim_train_vectors = train_vectors
+    #reduced_dim_train_vectors = train_vectors
 
     # Combine to split data set for ML
     train_tuples = np.array([(reduced_dim_train_vectors[i], train_labels[i]) for i in range(len(train_labels))])
